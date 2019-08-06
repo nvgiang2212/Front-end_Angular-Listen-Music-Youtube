b16fae48ceaad1bc7705a254ba892814040c07ee
vgiang <Giang221295>
2019-08-06 15:59:15 +0700

    second

--- src/app/youtube.service.ts
+++ src/app/youtube.service.ts
@@ -5,11 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class YoutubeService {
   playlist = [
-    {id: 'CX11yw6YL1w', song: 'Céline Dion - Ashes (from the Deadpool 2 Motion Picture Soundtrack)'},
-    {id: 'HBYirj2c_jw', song: 'Deadpool 2 - Take on Me'},
-    {id: 'N6O2ncUKvlg', song: 'Nelly - Just A Dream'},
-    {id: 'uelHwf8o7_U', song: 'Eminem - Love The Way You Lie ft. Rihanna'},
-    {id: 'WpYeekQkAdc', song: 'The Black Eyed Peas - Where Is The Love?'}
+    {id: 'U4xPahDjY6k', song: 'Lil Nas X - Old Town Road (Lyrics) ft. Billy Ray Cyrus'},
+    {id: 'hRXv_VU35Lw', song: 'Old Town Road - Walk off the Earth (Lil Nas X, Billy Ray Cyrus Cover)'},
+    {id: 'YTQ-n0SgdiY', song: 'Thịnh Suy - Thắc Mắc (MĐX) | Official Music Video'},
+    {id: 'vGJTaP6anOU', song: 'Elvis Presley - Can\'t Help Falling In Love (Audio)'},
+    {id: 'c86jOkPzsjk', song: '[Vietsub + Lyrics] The One That Got Away - Brielle Von Hugel | Katy Perry | Love Rosie'},
+    {id: 'BiKjeXNGCGU', song: 'Chuyện Người Anh Thương | Phạm Nguyên Ngọc | Original'},
+    {id: 'cBVGlBWQzuc', song: 'Maroon 5 - Girls Like You ft. Cardi B (Volume 2)'},
+    {id: '09R8_2nJtjg', song: 'Maroon 5 - Sugar'},
+    {id: 'IPfJnp1guPc', song: 'Khalid - Young Dumb & Broke (Official Music Video)'}
   ];
   constructor() { }
   find(id: string) {
